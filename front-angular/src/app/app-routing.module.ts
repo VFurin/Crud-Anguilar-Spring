3e02aecc7ff646fa0d2dd623f6060547cc6fd14f
VFurin <vinicius.furin@outlook.com>
2021-09-21 09:40:53 -0300

    Adicionando routing-module e customers.component

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'customers'},
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule )}
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
