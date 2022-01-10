f2162d33dcc4ed6f9d3a98a194747d74bf605dc4
VFurin <vinicius.furin@outlook.com>
2022-01-09 22:39:10 -0300

    Adicionando o customer model e repository

--- backend-spring/src/main/java/com/furin/backendspring/controller/CustomerController.java
+++ backend-spring/src/main/java/com/furin/backendspring/controller/CustomerController.java
@@ -1,5 +1,11 @@
 package com.furin.backendspring.controller;
 
+import java.util.List;
+
+import com.furin.backendspring.model.Customer;
+import com.furin.backendspring.repository.CustomerRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
-  
+
+  @Autowired
+  private CustomerRepository customerRepository;
+
   @GetMapping
-  public String customer(){
-    return "Andrea Medeiros";
+  public List<Customer> customerList() {
+    return customerRepository.findAll();
   }
 }
---
+++ backend-spring/src/main/java/com/furin/backendspring/model/Customer.java
@@ -0,0 +1,28 @@
+package com.furin.backendspring.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Customer {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 200, nullable = false)
+  private String name;
+  
+  @Column(length = 20, nullable = false)
+  private String phone;
+
+  @Column(length = 30, nullable = false)
+  private String status;
+
+}
